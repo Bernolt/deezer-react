ec33427007a28702e8cfdd56899f952e98e39bde
Koen Romers <info@koenromers.com>
2018-02-26 20:27:21 +0000

    feat(index): hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// Silence
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import './index.scss';
+// require('./favicon.ico'); // Tell webpack to load favicon.ico
+
+render(
+  <AppContainer>
+    <div>Hello world</div>
+  </AppContainer>,
+  document.getElementById('app')
+);

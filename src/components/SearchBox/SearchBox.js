5d3056866b27e4b5a65a9fd98557bd3e4a8f1b07
Koen Romers <info@koenromers.com>
2018-02-27 12:18:10 +0000

    feat(searchbox): setup basic component with test

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './SearchBox.scss';
+
+// Since this component is simple and static, there's no parent container for it.
+function SearchBox() {
+  return (
+    <div className="searchbox">Searchbox here</div>
+  );
+}
+
+export default SearchBox;
